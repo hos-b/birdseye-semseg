b82372bb523d6eba034db2ce0acbff77bea28c0f
hos-b <hexed@outlook.com>
2021-03-21 13:33:52 +0100

    add curriculum learning [untested]

--- scripts/agent/agent_pool.py
+++ scripts/agent/agent_pool.py
@@ -12,7 +12,8 @@ class CurriculumPool:
     training progresses, the number of agents that are allowed
     to propagate messages increases, starting with 1.
     """
-    def __init__(self, model: MassCNN, device: torch.device, starting_difficulty, maximum_difficulty, output_size) -> None:
+    def __init__(self, model: MassCNN, device: torch.device, output_size,
+                 starting_difficulty, maximum_difficulty, maximum_agent_count) -> None:
         self.model = model
         self.agent_count = 0
         self.device = device
@@ -26,39 +27,7 @@ class CurriculumPool:
         self.maximum_difficulty = maximum_difficulty
         self.combined_masks = None
         self.adjacency_matrix = None
-    
-    def generate_connection_strategy(self, ids, masks, transforms, pixels_per_meter, h, w, center_x, center_y):
-        """
-        combines all masks, find the best masks for each agent &
-        create adjacency matrix based on current difficulty
-        """
-        self.agent_count = masks.shape[0]
-        self.adjacency_matrix = torch.zeros((self.agent_count, self.agent_count), dtype=torch.bool)
-        ids = ids.squeeze()
-        # marking the masks
-        for i in range(len(ids.squeeze())):
-            masks[i] *= 1 << ids[i].item()
-        self.combined_masks = get_all_aggregate_masks(masks, transforms, pixels_per_meter, h, w, center_x, center_y).long()
-        # no calculations necessary for highest difficulty
-        if self.difficulty == self.maximum_difficulty:
-            self.combined_masks[self.combined_masks > 1] = 1
-            self.adjacency_matrix = torch.ones((self.agent_count, self.agent_count), dtype=torch.int8)
-            return
-        for i in range(self.agent_count):
-            possible_connections, counts = self.combined_mask[i].unique(sorted=True, return_counts=True)
-            possible_connections = possible_connections.long().tolist()
-            counts = counts.tolist()
-            considered_connections = 0
-            considered_connection_count = 0
-            while considered_connection_count < self.difficulty or len(possible_connections) > 0:
-                current_connection = possible_connections.pop(0)
-                # if already considered this agent
-                if (~considered_connections & current_connection) == 0:
-                    continue
-                # 
-                considered_connections |= current_connection
-                
-
+        self.max_agent_count = maximum_agent_count
                 
     def calculate_detached_messages(self, rgbs):
         self.agent_count = rgbs.shape[0]
@@ -107,9 +76,71 @@ class CurriculumPool:
         pooled_features = self.model.pyramid_pooling(aggregated_features.unsqueeze(0), self.output_size)
         return self.model.classifier(pooled_features)
 
-    def update_adjacency_matrix(self, agent_idx, mask_num, max_difficulty):
-        updated = 0 
-        for i in range(max_difficulty):
-            if mask_num & (1 << i) and self.adjacency_matrix[agent_idx, i]:
-                self.adjacency_matrix[agent_idx, i] = True
-                updated += 1
+    def generate_connection_strategy(self, ids, masks, transforms, pixels_per_meter, h, w, center_x, center_y):
+        """
+        combines all masks, find the best masks for each agent &
+        create adjacency matrix based on current difficulty
+        """
+        self.agent_count = masks.shape[0]
+        # also no calculations necessary for difficulty = 1
+        if self.difficulty ==  1:
+            self.combined_masks = masks
+            self.adjacency_matrix = torch.eye(self.agent_count, dtype=torch.bool)
+            return
+        # no calculations necessary for max possible difficulty (!= max_difficulty)
+        elif self.difficulty == self.max_agent_count:
+            self.combined_masks = get_all_aggregate_masks(masks, transforms, pixels_per_meter, h, w, center_x, center_y).long()
+            self.adjacency_matrix = torch.ones((self.agent_count, self.agent_count), dtype=torch.bool)
+            return
+        # for other cases, need to do some stuff
+        self.adjacency_matrix = torch.eye(self.agent_count, dtype=torch.bool)
+        ids = ids.squeeze()
+        # identifying the masks
+        for i in range(len(ids)):
+            masks[i] *= 1 << ids[i].item()
+        self.combined_masks = get_all_aggregate_masks(masks, transforms, pixels_per_meter, h, w,
+                                                      center_x, center_y, 'nearest').long()
+        # using the unique values of the mask to find agent view overlaps
+        for i in range(self.agent_count):
+            possible_connections, counts = self.combined_masks[i].unique(sorted=False, return_counts=True)
+            possible_connections = possible_connections.long().cpu().tolist()
+            counts = counts.cpu().tolist()
+            # get mask IDs sorted based on count
+            possible_connections = [x for _, x in sorted(zip(counts, possible_connections), reverse=True)]
+            possible_connections.remove(0)
+            # the ego-mask is always pre-selected
+            accepted_connections = 1 << i
+            accepted_connection_count = 1
+            # while 
+            while accepted_connection_count < self.difficulty or len(possible_connections) > 0:
+                current_connection = possible_connections.pop(0)
+                # if already accepted this connection earlier in another
+                if (~accepted_connections & current_connection) == 0:
+                    continue
+                # considering the constituent mask elements
+                for agent_idx in decompose_binary_elements(current_connection):
+                    # if agent has not been considered before
+                    if (agent_idx & ~accepted_connections):
+                        self.adjacency_matrix[i, agent_idx] = 1
+                        accepted_connections |= agent_idx
+                        accepted_connection_count += 1
+                        # not adding too many
+                        if accepted_connection_count == self.difficulty:
+                            break
+            # combining the mask of selected connections and setting everything else to 0
+            self.combined_masks[i] = self.combined_masks[i] & accepted_connections
+            self.combined_masks[i][self.combined_masks[i] != 0] = 1
+
+def decompose_binary_elements(mask_value) -> list:
+    """
+    as a part of generating a connection strategy, the mask value that has had
+    the highest overlap with the current agent is decomposed into its elements
+    Example: mask_value = 2 + 16 + 32 + 64 -> [1, 4, 5, 6]
+    """
+    elements = []
+    max_mask_value = 1
+    while mask_value >= max_mask_value:
+        if mask_value & max_mask_value:
+            elements.append(max_mask_value)
+        max_mask_value = max_mask_value << 1
+    return elements
