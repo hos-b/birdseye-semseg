c1bab3e8e329268d02311f4df08fc053e6dd7670
hos-b <hexed@outlook.com>
2021-04-14 22:49:35 +0200

    change instance norm due to bug

--- scripts/model/large_mcnn.py
+++ scripts/model/large_mcnn.py
@@ -26,16 +26,18 @@ class LMCNN(nn.Module):
                                                            out_channels=128,
                                                            scale_factor=4)
         self.classifier = Classifer(128, num_classes)
+        # none of the pyramid stages can be 1 due to instance norm issue
+        # https://github.com/pytorch/pytorch/issues/45687
         self.mask_global_feature_extractor = GlobalFeatureExtractor(in_channels=64,
                                                                     block_channels=(64, 96, 128),
                                                                     t=6,
                                                                     num_blocks=(3, 3, 3),
-                                                                    pool_sizes=(1, 2, 3, 6))
+                                                                    pool_sizes=(2, 3, 4, 5))
         self.mask_feature_fusion = FeatureFusionModule(highres_in_channels=64,
                                                        lowres_in_channels=128,
                                                        out_channels=128,
                                                        scale_factor=4)
-        self.maskifier = Classifer(128, num_classes)
+        self.maskifier = Classifer(128, 1)
 
     def forward(self, x, transforms, adjacency_matrix):
         # B, 3, 480, 640: input size
@@ -88,6 +90,136 @@ class LMCNN(nn.Module):
         """
         return sum(p.numel() for p in self.parameters() if p.requires_grad)
 
+class LearningToDownsample(nn.Module):
+    """Learning to downsample module"""
+
+    def __init__(self, dw_channels1=32, dw_channels2=48, out_channels=64):
+        super(LearningToDownsample, self).__init__()
+        self.conv = _ConvINReLU(in_channels=3, out_channels=dw_channels1, kernel_size=3, stride=2)
+        self.dsconv1 = _DSConv(dw_channels1, dw_channels2, 2)
+        self.dsconv2 = _DSConv(dw_channels2, out_channels, 2)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.dsconv1(x)
+        x = self.dsconv2(x)
+        return x
+
+class GlobalFeatureExtractor(nn.Module):
+    """Global feature extractor module"""
+
+    def __init__(self, in_channels=64, block_channels=(64, 96, 128),
+                 t=6, num_blocks=(3, 3, 3), pool_sizes=(1, 2, 3, 6)):
+        super(GlobalFeatureExtractor, self).__init__()
+        self.bottleneck1 = self._make_layer(LinearBottleneck, in_channels, block_channels[0], num_blocks[0], t, 2)
+        self.bottleneck2 = self._make_layer(LinearBottleneck, block_channels[0], block_channels[1], num_blocks[1], t, 2)
+        self.bottleneck3 = self._make_layer(LinearBottleneck, block_channels[1], block_channels[2], num_blocks[2], t, 1)
+        self.ppm = PyramidPooling(block_channels[2], block_channels[2], pool_sizes)
+
+    def _make_layer(self, block, inplanes, planes, blocks, t=6, stride=1):
+        layers = []
+        layers.append(block(inplanes, planes, t, stride))
+        for i in range(1, blocks):
+            layers.append(block(planes, planes, t, 1))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.bottleneck1(x)
+        x = self.bottleneck2(x)
+        x = self.bottleneck3(x)
+        x = self.ppm(x)
+        return x
+
+class FeatureFusionModule(nn.Module):
+    """Feature fusion module"""
+
+    def __init__(self, highres_in_channels, lowres_in_channels, out_channels, scale_factor=4):
+        super(FeatureFusionModule, self).__init__()
+        self.scale_factor = scale_factor
+        self.dwconv = _DWConv(lowres_in_channels, out_channels, 1)
+        self.conv_lower_res = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, 1),
+            nn.InstanceNorm2d(out_channels)
+        )
+        self.conv_higher_res = nn.Sequential(
+            nn.Conv2d(highres_in_channels, out_channels, 1),
+            nn.InstanceNorm2d(out_channels)
+        )
+        self.relu = nn.ReLU(True)
+
+    def forward(self, higher_res_feature, lower_res_feature):
+        lower_res_feature = F.interpolate(lower_res_feature, scale_factor=4, mode='bilinear', align_corners=True)
+        lower_res_feature = self.dwconv(lower_res_feature)
+        lower_res_feature = self.conv_lower_res(lower_res_feature)
+
+        higher_res_feature = self.conv_higher_res(higher_res_feature)
+        out = higher_res_feature + lower_res_feature
+        return self.relu(out)
+
+class Classifer(nn.Module):
+    """Classifer"""
+
+    def __init__(self, dw_channels, num_classes, stride=1):
+        super(Classifer, self).__init__()
+        self.dsconv1 = _DSConv(dw_channels, dw_channels, stride)
+        self.dsconv2 = _DSConv(dw_channels, dw_channels, stride)
+        self.conv = nn.Sequential(
+            nn.Dropout(0.1),
+            nn.Conv2d(dw_channels, num_classes, 1)
+        )
+
+    def forward(self, x):
+        x = self.dsconv1(x)
+        x = self.dsconv2(x)
+        x = self.conv(x)
+        return x
+
+class LinearBottleneck(nn.Module):
+    """LinearBottleneck used in MobileNetV2"""
+    def __init__(self, in_channels, out_channels, t=6, stride=2):
+        super(LinearBottleneck, self).__init__()
+        self.use_shortcut = stride == 1 and in_channels == out_channels
+        self.block = nn.Sequential(
+            # pw
+            _ConvINReLU(in_channels, in_channels * t, 1),
+            # dw
+            _DWConv(in_channels * t, in_channels * t, stride),
+            # pw-linear
+            nn.Conv2d(in_channels * t, out_channels, 1, bias=False),
+            nn.InstanceNorm2d(out_channels)
+        )
+
+    def forward(self, x):
+        out = self.block(x)
+        if self.use_shortcut:
+            out = x + out
+        return out
+
+class PyramidPooling(nn.Module):
+    """Pyramid pooling module"""
+    def __init__(self, in_channels, out_channels, pool_sizes):
+        super(PyramidPooling, self).__init__()
+        stage_count = len(pool_sizes)
+        assert in_channels % stage_count == 0, f'{in_channels} is not divisble by number of pooling stages {stage_count}'
+        intermediate_channels = in_channels // stage_count
+        self.stages = nn.ModuleList([self._make_stage(in_channels, intermediate_channels, size) for size in pool_sizes])
+        self.bottleneck = _ConvINReLU(in_channels * 2, out_channels, 1)
+
+    def _make_stage(self, in_channels, out_channels, avgpool_size):
+        return nn.Sequential(
+            nn.AdaptiveAvgPool2d(avgpool_size),
+            _ConvINReLU(in_channels, out_channels, kernel_size=1)
+        )
+
+    def upsample(self, x, size):
+        return F.interpolate(x, size, mode='bilinear', align_corners=True)
+
+    def forward(self, x):
+        size = x.size()[2:]
+        pooled_features = [F.interpolate(stage(x), size=size, mode='bilinear', align_corners=True) for stage in self.stages]
+        x = torch.cat(pooled_features + [x], dim=1)
+        return self.bottleneck(x)
+
 class _ConvINReLU(nn.Module):
     """Conv-BN-ReLU"""
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0):
@@ -128,139 +260,3 @@ class _DWConv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
-
-class LinearBottleneck(nn.Module):
-    """LinearBottleneck used in MobileNetV2"""
-    def __init__(self, in_channels, out_channels, t=6, stride=2):
-        super(LinearBottleneck, self).__init__()
-        self.use_shortcut = stride == 1 and in_channels == out_channels
-        self.block = nn.Sequential(
-            # pw
-            _ConvINReLU(in_channels, in_channels * t, 1),
-            # dw
-            _DWConv(in_channels * t, in_channels * t, stride),
-            # pw-linear
-            nn.Conv2d(in_channels * t, out_channels, 1, bias=False),
-            nn.InstanceNorm2d(out_channels)
-        )
-
-    def forward(self, x):
-        out = self.block(x)
-        if self.use_shortcut:
-            out = x + out
-        return out
-
-class PyramidPooling(nn.Module):
-    """Pyramid pooling module"""
-    def __init__(self, in_channels, out_channels, pool_sizes):
-        super(PyramidPooling, self).__init__()
-        stage_count = len(pool_sizes)
-        assert in_channels % stage_count == 0, f'{in_channels} is not divisble by number of pooling stages {stage_count}'
-        intermediate_channels = in_channels // stage_count
-        self.stages = [self._make_stage(in_channels, intermediate_channels, size) for size in pool_sizes]
-        self.bottleneck = _ConvINReLU(in_channels * 2, out_channels, 1)
-
-    def _make_stage(self, in_channels, out_channels, avgpool_size):
-        return nn.Sequential(
-            nn.AdaptiveAvgPool2d(avgpool_size),
-            _ConvINReLU(in_channels, out_channels, kernel_size=1)
-        )
-
-    def upsample(self, x, size):
-        return F.interpolate(x, size, mode='bilinear', align_corners=True)
-
-    def forward(self, x):
-        size = x.size()[2:]
-        pooled_features = [F.interpolate(stage(x), size=size, mode='bilinear',
-                           align_corners=True) for stage in self.stages]
-        x = torch.cat(pooled_features + [x], dim=1)
-        return self.bottleneck(x)
-
-
-class LearningToDownsample(nn.Module):
-    """Learning to downsample module"""
-
-    def __init__(self, dw_channels1=32, dw_channels2=48, out_channels=64):
-        super(LearningToDownsample, self).__init__()
-        self.conv = _ConvINReLU(in_channels=3, out_channels=dw_channels1, kernel_size=3, stride=2)
-        self.dsconv1 = _DSConv(dw_channels1, dw_channels2, 2)
-        self.dsconv2 = _DSConv(dw_channels2, out_channels, 2)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.dsconv1(x)
-        x = self.dsconv2(x)
-        return x
-
-
-class GlobalFeatureExtractor(nn.Module):
-    """Global feature extractor module"""
-
-    def __init__(self, in_channels=64, block_channels=(64, 96, 128),
-                 t=6, num_blocks=(3, 3, 3), pool_sizes=(1, 2, 3, 6)):
-        super(GlobalFeatureExtractor, self).__init__()
-        self.bottleneck1 = self._make_layer(LinearBottleneck, in_channels, block_channels[0], num_blocks[0], t, 2)
-        self.bottleneck2 = self._make_layer(LinearBottleneck, block_channels[0], block_channels[1], num_blocks[1], t, 2)
-        self.bottleneck3 = self._make_layer(LinearBottleneck, block_channels[1], block_channels[2], num_blocks[2], t, 1)
-        self.ppm = PyramidPooling(block_channels[2], block_channels[2], pool_sizes)
-
-    def _make_layer(self, block, inplanes, planes, blocks, t=6, stride=1):
-        layers = []
-        layers.append(block(inplanes, planes, t, stride))
-        for i in range(1, blocks):
-            layers.append(block(planes, planes, t, 1))
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.bottleneck1(x)
-        x = self.bottleneck2(x)
-        x = self.bottleneck3(x)
-        x = self.ppm(x)
-        return x
-
-
-class FeatureFusionModule(nn.Module):
-    """Feature fusion module"""
-
-    def __init__(self, highres_in_channels, lowres_in_channels, out_channels, scale_factor=4):
-        super(FeatureFusionModule, self).__init__()
-        self.scale_factor = scale_factor
-        self.dwconv = _DWConv(lowres_in_channels, out_channels, 1)
-        self.conv_lower_res = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, 1),
-            nn.InstanceNorm2d(out_channels)
-        )
-        self.conv_higher_res = nn.Sequential(
-            nn.Conv2d(highres_in_channels, out_channels, 1),
-            nn.InstanceNorm2d(out_channels)
-        )
-        self.relu = nn.ReLU(True)
-
-    def forward(self, higher_res_feature, lower_res_feature):
-        lower_res_feature = F.interpolate(lower_res_feature, scale_factor=4, mode='bilinear', align_corners=True)
-        lower_res_feature = self.dwconv(lower_res_feature)
-        lower_res_feature = self.conv_lower_res(lower_res_feature)
-
-        higher_res_feature = self.conv_higher_res(higher_res_feature)
-        out = higher_res_feature + lower_res_feature
-        return self.relu(out)
-
-
-class Classifer(nn.Module):
-    """Classifer"""
-
-    def __init__(self, dw_channels, num_classes, stride=1):
-        super(Classifer, self).__init__()
-        self.dsconv1 = _DSConv(dw_channels, dw_channels, stride)
-        self.dsconv2 = _DSConv(dw_channels, dw_channels, stride)
-        self.conv = nn.Sequential(
-            nn.Dropout(0.1),
-            nn.Conv2d(dw_channels, num_classes, 1)
-        )
-
-    def forward(self, x):
-        x = self.dsconv1(x)
-        x = self.dsconv2(x)
-        x = self.conv(x)
-        return x
-
