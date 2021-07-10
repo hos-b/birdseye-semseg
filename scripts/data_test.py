37e1953b6234a5a3a1e42c0264d9348b01a241dc
hos-b <hexed@outlook.com>
2021-07-10 16:29:02 +0200

    remove batch idx from loader

--- scripts/data_test.py
+++ scripts/data_test.py
@@ -24,7 +24,7 @@ dset = MassHDF5(dataset=train_cfg.trainset_name, path=train_cfg.dset_dir,
 loader = torch.utils.data.DataLoader(dset, batch_size=1, shuffle=False, num_workers=1)
 # plot stuff
 columns = 6
-for idx, (_, rgbs, semsegs, masks, car_transforms, _) in enumerate(loader):
+for idx, (rgbs, semsegs, masks, car_transforms, _) in enumerate(loader):
     # randomly skip samples (useful for large datasets)
     if show_random_samples and bool(random.randint(0, 1)):
         continue
