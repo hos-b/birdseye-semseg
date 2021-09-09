b7ee4d1e917b4dfcd5c97ab52f8e9c359a785443
hos-b <hexed@outlook.com>
2021-09-09 14:06:53 +0200

    remove all tensorboard depnedencies

--- scripts/data/logging.py
+++ scripts/data/logging.py
@@ -1,27 +1,5 @@
 import wandb
-from tensorboardX import SummaryWriter
-
 from data.config import TrainingConfig
-
-def log_wandb(rank: int, data: dict):
-    if rank != 0:
-        return
-    wandb.log(data)
-
-def log_scalar_tb(rank: int, scalar, key: 'str', idx: int, writer: SummaryWriter):
-    if rank != 0:
-        return
-    writer.add_scalar(key, scalar, idx)
-
-def log_image_tb(rank: int, image, key:str, idx: int, writer: SummaryWriter):
-    if rank != 0:
-        return
-    writer.add_image(key, image, idx)
-
-def log_string(rank: int, message: str, end: str = '\n'):
-    if rank == 0:
-        print(message, end=end)
-
 
 def init_wandb(name: str, train_cfg: TrainingConfig):
     wandb.init(
