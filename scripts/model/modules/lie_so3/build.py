d657245d76869f77537d48bb7627b9c3d21bb1c2
hos-b <hexed@outlook.com>
2021-09-16 17:31:42 +0200

    update build script

--- scripts/model/modules/lie_so3/build.py
+++ scripts/model/modules/lie_so3/build.py
@@ -1,14 +1,5 @@
-# https://gist.github.com/tonyseek/7821993
-import cv2
-import glob
-from os import path as osp
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
-
-abs_path = osp.dirname(osp.realpath(__file__))
-extra_objects = [osp.join(abs_path, 'build/lie_so3.so')]
-extra_objects += glob.glob('/usr/local/cuda/lib64/*.a')
-
 
 setup(
     name='lie_so3',
