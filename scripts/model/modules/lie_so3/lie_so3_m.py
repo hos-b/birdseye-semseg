1eae7347999e5367190abefe284d9cb03077953a
hos-b <hexed@outlook.com>
2021-10-28 19:03:42 +0200

    add original version of lie se3

--- scripts/model/modules/lie_so3/lie_so3_m.py
+++ scripts/model/modules/lie_so3/lie_so3_m.py
@@ -42,6 +42,73 @@ class LieSE3(torch.nn.Module):
 
         return complete_transformation
 
+class OrgLieSE3(torch.nn.Module):
+    """
+        computes the Lie transform based on 3 given parameters
+        estimated from a nerual network. the transform is computed
+        per sample in the batch. the output is a 4x4 matrix.
+    """
+    def __init__(self, translation_scale=1, rotation_scale=0.1, eps=1e-5):
+        super(OrgLieSE3, self).__init__()
+        self.translation_scale = translation_scale
+        self.rotation_scale = rotation_scale
+        self.eps = eps
+
+    def forward(self, uv):
+        """
+        uv -> Bx3: [x, y, p_yaw]
+        """
+        batch_size = uv.size(0)
+        device = uv.device
+        complete_transformation = torch.zeros((batch_size, 4, 4)).to(device)
+        complete_transformation[:, 3, 3] = 1.
+
+        for b in range(batch_size):
+            omega_x = 0
+            omega_y = 0
+            omega_z = uv[b, 2] * self.rotation_scale
+
+            u_x = uv[b, 0] * self.translation_scale
+            u_y = uv[b, 1] * self.translation_scale
+            u_z = 0
+
+            omega_skew = torch.zeros(3, 3).to(device)
+
+            omega_skew[0, 1] = -omega_z
+            omega_skew[0, 2] = omega_y
+            omega_skew[1, 0] = omega_z
+            omega_skew[1, 2] = -omega_x
+            omega_skew[2, 0] = -omega_y
+            omega_skew[2, 1] = omega_x
+
+            u_vec = torch.zeros((3, 1)).to(device)
+            u_vec[0, :] = u_x
+            u_vec[1, :] = u_y
+            u_vec[2, :] = u_z
+
+            omega_skew_squared = torch.mm(omega_skew, omega_skew)
+
+            theta_sqr = omega_x * omega_x + omega_y * omega_y + omega_z * omega_z
+
+            theta = torch.sqrt(theta_sqr)
+
+            sin_theta = torch.sin(theta)
+            sin_theta_div_theta = sin_theta / (theta + self.eps)
+
+            one_minus_cos_theta = 1 - torch.cos(theta)
+            one_minus_cos_div_theta_sqr = one_minus_cos_theta / (theta_sqr + self.eps)
+
+            # SO(3) rotation
+            one_minus_A_div_theta_sqr = (1. - sin_theta_div_theta) / (theta_sqr + self.eps)
+
+            complete_transformation[b, 0:3, 0:3] = torch.eye(3).to(device) + torch.mul(omega_skew, sin_theta_div_theta) + torch.mul(omega_skew_squared, one_minus_cos_div_theta_sqr)
+
+            tmp = (torch.eye(3).to(device) + torch.mul(omega_skew, one_minus_cos_div_theta_sqr) + torch.mul(
+                omega_skew_squared, one_minus_A_div_theta_sqr))
+            complete_transformation[b, 0:3, 3] = torch.mm(tmp, u_vec).squeeze()
+
+        return complete_transformation
+
 
 class LieSE2(torch.nn.Module):
     """
